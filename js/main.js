4a400d95ab502ba25fd5e320c710a22c2279fbf8
Артем <Azart282018@yandex.ru>
2020-10-12 21:44:36 +0300

    +

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ const getRandomPhotos = function (count) {
 };
 
 
-const getRandomNumber = function(min, max) {
+const getRandomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
